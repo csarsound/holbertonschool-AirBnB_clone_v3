7fbba31702a106d4a4a76b8a0cf423322993c183
csarsound <csarsound@hotmail.com>
2022-09-20 16:19:46 -0500

    SomeStats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,20 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views_route('/status', methods=['GET'])
 def code_status():
     """show status of the code"""
     return jsonify({"status": "OK"})
+
+
+@app_vewvs.route('/stats', methods=['GET'])
+def code_stats():
+    """show stats about the classes"""
+    classes= {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
+              'reviews': 'Review', 'states': 'State', 'users': 'User'}
+
+    d = {p_cls:storage.count(r_cls) for p_cls, r_cls in classes.items()}
+    return jsonify(d)
